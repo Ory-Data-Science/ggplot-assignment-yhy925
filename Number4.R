fce0c19cfc295cf4be27eb6458df6431288d7c21
Jeramia Ory <jeramia.ory@gmail.com>
2017-10-25 10:14:47 -0500

    Create Number4.R

---
+++ Number4.R
@@ -0,0 +1,8 @@
+#load library
+library(tidyverse)
+
+#load data from the internet, using tabs to seperate and replacing -999, etc with NA's 
+dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan07.txt"),
+                sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
+
+#your code goes here
