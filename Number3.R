ab507f132cdb89be1ae5807a077ee66f9ba06534
Jeramia Ory <jeramia.ory@gmail.com>
2017-10-25 10:10:52 -0500

    Create Number3.R

---
+++ Number3.R
@@ -0,0 +1,8 @@
+#load library
+library(tidyverse)
+
+#load data
+dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt"), 
+                sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
+
+#rest of your code goes here
